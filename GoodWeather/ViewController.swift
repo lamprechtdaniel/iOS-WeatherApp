9eb03f12043b2dc48b6390bbe243f511d0695a75
Daniel Lamprecht <lamprecht.daniel@outlook.com>
2019-06-28 12:24:15 +0200

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Daniel Lamprecht on 28.06.19.
+//  Copyright © 2019 Daniel Lamprecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
